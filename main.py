baa93e168bffba07c24ee0df9009a48b1e2d166e
Zemke <flzemke@gmail.com>
2025-11-29 03:11:50 +0100

    split

--- main.py
+++ main.py
@@ -2,60 +2,11 @@
 
 import torch
 
-from torchvision.transforms.v2.functional import InterpolationMode
-from torchvision.transforms.v2 import RandomRotation
 from torchvision.transforms import v2 as T
 from torchvision.transforms.v2 import functional as F
 from torchvision.utils import make_grid
 
-from PIL import Image
-import numpy as np
-
-from math import cos, sin, pi
-
-
-class RandomRotationFit(RandomRotation):
-
-  def __init__(
-    self,
-    degrees: Union[numbers.Number, Sequence],
-    interpolation: Union[InterpolationMode, int] = InterpolationMode.NEAREST,
-    expand: bool = False,
-    center: Optional[list[float]] = None,
-    fill: Union[_FillType, dict[Union[type, str], _FillType]] = 0,
-  ) -> None:
-    super().__init__(
-      degrees=degrees,
-      interpolation=interpolation,
-      expand=True,
-      center=None,
-      fill=fill
-    )
-
-  def transform(self, inpt: Any, params: dict[str, Any]) -> Any:
-    if params['angle'] == 0.:
-      return inpt
-    _, H, W = inpt.shape
-    r = (H if H < W else W)//2
-    I = super().transform(inpt, params)
-    deg = params['angle'] * (pi / 180)
-    _, h, w = I.shape
-    ww, hh = [], []
-    for x,y in [
-      (-(W//2)+r, -(H//2)+r),
-      (W - r - W//2, H - r - H//2)
-    ]:
-      if params['angle'] == 90.:
-        x1, y1 = -y, x
-      if params['angle'] == 180.:
-        x1, y1 = -x, -y
-      if params['angle'] == 270.:
-        x1, y1 = y, -x
-      else:
-        x1, y1 = x*cos(deg) - y*sin(deg), x*sin(deg) + y*cos(deg)
-      ww.append(int(w//2+x1))
-      hh.append(int(h//2-y1))
-    return I[:, max([0, min(hh)-r]):max(hh)+r, max([0, min(ww)-r]):max(ww)+r]
+from rotate_fit import RandomRotationFit
 
 
 if __name__ == '__main__':
