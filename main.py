154c2ee5f8f1ad32f366e875be23c2be20522fba
Zemke <flzemke@gmail.com>
2025-11-29 03:09:24 +0100

    quick square angles

--- main.py
+++ main.py
@@ -33,24 +33,29 @@ class RandomRotationFit(RandomRotation):
     )
 
   def transform(self, inpt: Any, params: dict[str, Any]) -> Any:
+    if params['angle'] == 0.:
+      return inpt
     _, H, W = inpt.shape
     r = (H if H < W else W)//2
     I = super().transform(inpt, params)
     deg = params['angle'] * (pi / 180)
     _, h, w = I.shape
-    ww = []
-    hh = []
+    ww, hh = [], []
     for x,y in [
       (-(W//2)+r, -(H//2)+r),
       (W - r - W//2, H - r - H//2)
     ]:
-      x1 = x*cos(deg) - y*sin(deg)
-      y1 = x*sin(deg) + y*cos(deg)
-      h1 = int(h//2-y1)
-      w1 = int(w//2+x1)
-      ww.append(w1)
-      hh.append(h1)
-    return I[:, min(hh)-r:max(hh)+r, min(ww)-r:max(ww)+r]
+      if params['angle'] == 90.:
+        x1, y1 = -y, x
+      if params['angle'] == 180.:
+        x1, y1 = -x, -y
+      if params['angle'] == 270.:
+        x1, y1 = y, -x
+      else:
+        x1, y1 = x*cos(deg) - y*sin(deg), x*sin(deg) + y*cos(deg)
+      ww.append(int(w//2+x1))
+      hh.append(int(h//2-y1))
+    return I[:, max([0, min(hh)-r]):max(hh)+r, max([0, min(ww)-r]):max(ww)+r]
 
 
 if __name__ == '__main__':
